2afeb162172ef050362afc4960068086dbaff4f9
Vlad <malosay21@gmail.com>
2021-02-04 08:39:46 +0300

    Добавьте файлы проекта.

---
+++ UP_Laba6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UP_Laba6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
